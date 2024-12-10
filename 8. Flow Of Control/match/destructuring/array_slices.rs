fc00452ad85edbcb448a58f6565d51936844b4a5
Subhadeep Banerjee <subhadeep762@gmail.com>
2024-12-11 01:57:14 +0530

    Add Match Array Destructuring

---
+++ 8. Flow Of Control/match/destructuring/array_slices.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let array = [1, -2, 6];
+    println!("{:?}", array);
+
+    match array {
+        // Bins the second and the third elements to the respective variables
+        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values cab be ignores with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
