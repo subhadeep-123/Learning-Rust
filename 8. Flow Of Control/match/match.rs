e937158222f89de565452ada55eb7b9bde1a15f6
Subhadeep Banerjee <subhadeep762@gmail.com>
2024-12-11 01:49:55 +0530

    Add Match

---
+++ 8. Flow Of Control/match/match.rs
@@ -0,0 +1,20 @@
+// Rust provide pattern matching with `match` keyword
+fn main() {
+    let number = 13;
+
+    println!("Tell me about {}", number);
+
+    match number {
+        1 => println!("One"),
+        2 | 3 | 5 | 7 | 11 => println!("Prime"),
+        13..=19 => println!("A teen"),
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+    let bin = match boolean {
+        false => 0,
+        true => 1,
+    };
+    println!("{} -> {}", boolean, bin);
+}
