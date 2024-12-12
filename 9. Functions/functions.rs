12988d93c9d59cb80b412a747d5f5922c89158d2
Subhadeep Banerjee <subhadeep762@gmail.com>
2024-12-13 01:19:39 +0530

    Add functions, Associated functions and methods

---
+++ 9. Functions/functions.rs
@@ -0,0 +1,33 @@
+fn main() {
+    frizzbuzz_to(100);
+}
+
+// Function that returns boolean value
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+
+    lhs % rhs == 0
+}
+
+// Functions that "don't" return a value, actually return the unit type `()`
+fn fizzbuzz(n: u32) -> () {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else if is_divisible_by(n, 5) {
+        println!("buzz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+// When a function returns `()`, the return type can be omitted from the
+// signature
+fn fizzbuzz_to(n: u32) {
+    for n in 1..=n {
+        fizzbuzz(n);
+    }
+}
