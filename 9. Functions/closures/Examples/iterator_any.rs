a828b110f1ae8eb21d45fff7702be27ebeb75ded
Subhadeep Banerjee <subhadeep762@gmail.com>
2024-12-15 02:53:29 +0530

    Add Iterators example

---
+++ 9. Functions/closures/Examples/iterator_any.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vectors yields `&i32`. Destructure to `i32`
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+
+    // `into_iter()` for vectors yields `&i32`. NO destructure to `i32`
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+}
