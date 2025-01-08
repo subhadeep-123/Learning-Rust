54efda04221053bf0daefb15407fe40329f70d5e
Subhadeep Banerjee <subhadeep762@gmail.com>
2025-01-09 01:52:09 +0530

    Lifetime inprogress

---
+++ 15. Scoping Rules/lifetime/lifetime.rs
@@ -0,0 +1,19 @@
+// `i` has the longest lifetime because its scope entirely encloses
+// both `borrow1` and `borrow2`. The duration of `borrow1` compared to
+// `borrow2` is irrelevant since they are disjoint
+fn main() {
+    let i = 3; // Lifetime for `i` starts.
+    {
+        let borrow1 = &i; // `borrow1` lifetime starts.
+
+        println!("borrow1: {}", borrow1);
+    } // `borrow1` ends
+
+    {
+        let borrow2 = &i; // `borrow2` lifetime  starts.
+
+        println!("borrow2: {}", borrow2);
+    } // `borrow2` ends.
+
+    // Lifetime of i ends.
+}
